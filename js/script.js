dfb3b03d307ba0a772862d0f5c0e9ef541006381
gmlrnd <gumalaranda12@gmail.com>
2023-12-29 16:02:48 +0700

    js/script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 function hitung(){
-    let tinggi = document.getElementById("input-tinggi").value;
-    let alas = document.getElementById("input-alas").value;
+    var tinggi = document.getElementById("input-tinggi").value;
+    if (tinggi == "") { 
+        alert("input tinggi") ;
+        return false;
+    } 
+    var alas = document.getElementById("input-alas").value;
+    if (alas == "") { 
+        alert("input alas") ;
+        return false;
+    } 
     let hasil = (parseInt(alas) * (0.5)) * parseInt(tinggi);
     hasil = document.getElementById("hasil").value;
     document.getElementById("hasil").value = (parseInt(alas) * (0.5)) * parseInt(tinggi);
+    
+
+    
     console.log (hasil)
+    
+        
 }
 
 function reset(){
@@ -18,8 +31,20 @@ function reset(){
 
 function hitungk(){
     let sisi1 = document.getElementById("input-sisi-1").value;
+    if (sisi1 == "") { 
+        alert("input sisi 1") ;
+        return false;
+    } 
     let sisi2 = document.getElementById("input-sisi-2").value;
+    if (sisi2 == "") { 
+        alert("input sisi 2") ;
+        return false;
+    } 
     let sisi3 = document.getElementById("input-sisi-3").value;
+    if (sisi3 == "") { 
+        alert("input sisi 3") ;
+        return false;
+    } 
     let hasil = parseInt(sisi1)+ parseInt(sisi2) + parseInt(sisi3);
     hasil = document.getElementById("hasilk").value;
     document.getElementById("hasilk").value = parseInt(sisi1)+ parseInt(sisi2) + parseInt(sisi3);
